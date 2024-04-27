0552cf301c59d425dbff4f84175cd561b0488b4e
vytorrennan <crosdeadbr@gmail.com>
2024-04-26 22:01:18 -0300

    innitial commit

---
+++ textToSpeech.py
@@ -0,0 +1,32 @@
+from os import system
+from slugify import slugify
+from bs4 import BeautifulSoup
+import ebooklib
+from ebooklib import epub
+
+
+def chapterToString(chapter):
+    soup = BeautifulSoup(chapter.get_body_content(), "html.parser")
+    text = [p.get_text() for p in soup.find_all("p")]
+    return (" ".join(text)).replace(";", "").replace('"', '\\"')
+
+
+book = epub.read_epub("DesmistificandoAlgoritmosThomasHCormen2013.epub")
+chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+
+chapterStrings = []
+for c in chapters:
+    chapterStrings.append(chapterToString(c))
+
+#text = open("text.txt", "r").read()
+currentPart = 0
+totalOfParts = len(chapterStrings) - 1
+for text in chapterStrings:
+    currentPart = currentPart + 1
+    if currentPart == totalOfParts:
+        text = text + ". ESTE É O FIM DO TEXTO."
+    filename = f"part{currentPart}"
+
+    system(f"echo '{text}' | piper \
+  --model pt_BR-faber-medium \
+  --output_file audiobook/{filename}.wav")
